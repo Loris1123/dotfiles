dbfcfb9eafab3b3d4c3a82609687632982b308dd
Loris1123 <molottermann@gmail.com>
2020-09-30 09:24:51 +0200

    Switch vom Compton to Picom

--- .config/qtile/autostart_once.sh
+++ .config/qtile/autostart_once.sh
@@ -3,5 +3,5 @@
 XSECURELOCK_NO_COMPOSITE=1 XSECURELOCK_LIST_VIDEOS_COMMAND="ls ~/.dotfiles/screensaver.webm" XSECURELOCK_SAVER=saver_mpv XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 /usr/bin/xss-lock -n /usr/lib/xsecurelock/dimmer -l -- /usr/bin/xsecurelock &
 /usr/bin/nm-applet &
 /usr/bin/pasystray &
-/usr/bin/compton &
+/usr/bin/picom &
 /usr/bin/dunst &
